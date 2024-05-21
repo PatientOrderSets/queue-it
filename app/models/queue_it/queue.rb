60d1b9d0ea12b92bd8cf39a4d727c490497d466a
Johnny Hsiao <johnny.hsiao@thinkresearch.com>
2024-05-21 17:38:12 -0400

    Safely call methods

--- app/models/queue_it/queue.rb
+++ app/models/queue_it/queue.rb
@@ -28,13 +28,13 @@ module QueueIt
     end
 
     def get_next_by_with_queue_length_one(nodable_attribute, attribute_value)
-      head_node if head_node.nodable.send(nodable_attribute) == attribute_value
+      head_node if head_node&.nodable.send(nodable_attribute) == attribute_value
     end
 
     def get_next_by_with_queue_length_two(nodable_attribute, attribute_value)
-      if head_node.nodable.send(nodable_attribute) == attribute_value
+      if head_node&.nodable.send(nodable_attribute) == attribute_value
         get_next_in_queue_with_length_two
-      elsif tail_node.nodable.send(nodable_attribute) == attribute_value
+      elsif tail_node&.nodable.send(nodable_attribute) == attribute_value
         tail_node
       end
     end
@@ -42,8 +42,8 @@ module QueueIt
     def get_next_in_queue_with_length_two
       ActiveRecord::Base.transaction do
         lock!
-        old_head_node = head_node.lock!
-        old_tail_node = tail_node.lock!
+        old_head_node = head_node&.lock!
+        old_tail_node = tail_node&.lock!
         nodes.where.not(kind: :any).find_each { |node| node.update!(kind: :any) }
         old_head_node.update!(kind: :tail, parent_node: old_tail_node)
         old_tail_node.update!(kind: :head, parent_node: nil)
@@ -52,7 +52,7 @@ module QueueIt
     end
 
     def get_next_by_in_generic_queue(nodable_attribute, attribute_value)
-      if head_node.nodable.send(nodable_attribute) == attribute_value
+      if head_node&.nodable.send(nodable_attribute) == attribute_value
         return get_next_in_queue_generic
       end
 
@@ -70,9 +70,9 @@ module QueueIt
     def get_next_in_queue_generic
       ActiveRecord::Base.transaction do
         lock!
-        old_head_node = head_node.lock!
-        old_second_node = old_head_node.child_node.lock!
-        old_tail_node = tail_node.lock!
+        old_head_node = head_node&.lock!
+        old_second_node = old_head_node.child_node&.lock!
+        old_tail_node = tail_node&.lock!
         nodes.where.not(kind: :any).find_each { |node| node.update!(kind: :any) }
         old_head_node.update!(kind: :tail, parent_node: old_tail_node)
         old_second_node.update!(kind: :head, parent_node: nil)
@@ -107,7 +107,7 @@ module QueueIt
     def push_in_head(nodable)
       ActiveRecord::Base.transaction do
         lock!
-        old_head_node = head_node.lock!
+        old_head_node = head_node&.lock!
         kind = one_node? ? :tail : :any
         old_head_node.update!(kind: kind)
         new_head_node = nodes.create!(nodable: nodable, kind: :head)
@@ -122,7 +122,7 @@ module QueueIt
     def push_in_tail(nodable)
       ActiveRecord::Base.transaction do
         lock!
-        old_tail_node = tail_node.lock!
+        old_tail_node = tail_node&.lock!
         old_tail_node.update!(kind: :any)
         nodes.create!(nodable: nodable, kind: :tail, parent_node: old_tail_node)
       end
@@ -135,10 +135,10 @@ module QueueIt
     def move_current_node(current_node)
       ActiveRecord::Base.transaction do
         lock!
-        old_parent_node = current_node.parent_node.lock!
-        old_current_node = current_node.lock!
-        old_next_node = current_node.child_node.lock!
-        old_tail_node = tail_node.lock!
+        old_parent_node = current_node.parent_node&.lock!
+        old_current_node = current_node&.lock!
+        old_next_node = current_node.child_node&.lock!
+        old_tail_node = tail_node&.lock!
         old_tail_node.update!(kind: :any)
         old_current_node.update!(kind: :tail, parent_node: old_tail_node)
         old_next_node.update!(parent_node: old_parent_node)
